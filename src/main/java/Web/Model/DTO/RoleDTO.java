ac7111d9dca9b0b8578c60a5aa5ad26cfff1a211
MarwaBraiki <braikimarwa91@gmail.com>
2024-08-27 17:14:36 +0200

    [Task] Application of modification stash

---
+++ src/main/java/Web/Model/DTO/RoleDTO.java
@@ -0,0 +1,77 @@
+package Web.Model.DTO;
+
+import Entities.Business.Film.Film;
+import Entities.Business.Personne.Personne;
+import Entities.Business.Role.Role;
+
+public class RoleDTO {
+
+    private Long id;
+    private String roleName;
+    private Film film;
+    private Personne actor;
+
+    // Default constructor
+    public RoleDTO() {}
+
+    // Parameterized constructor
+    public RoleDTO(Long id, String roleName, Film film, Personne actor) {
+        this.id = id;
+        this.roleName = roleName;
+        this.film = film;
+        this.actor = actor;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public Personne getActor() {
+        return actor;
+    }
+
+    public void setActor(Personne actor) {
+        this.actor = actor;
+    }
+
+    // Static method to convert a Role entity to a RoleDTO
+    public static RoleDTO fromEntity(Role role) {
+        return new RoleDTO(
+                role.getId(),
+                role.getRoleName(),
+                role.getFilm(),
+                role.getActor()
+        );
+    }
+
+    // Convert RoleDTO to Role entity
+    public Role toEntity() {
+        Role role = new Role();
+        role.setId(this.id);
+        role.setRoleName(this.roleName);
+        role.setFilm(this.film);
+        role.setActor(this.actor);
+        return role;
+    }
+}
