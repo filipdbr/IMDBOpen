b23b79d30c62931a0c83dba5a6f827e36800c756
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-08-27 15:46:39 +0200

    [Task] Implementation de classe personne
     1) Fix Acteur Entity
     2)  Classe Abstraite personne

--- src/main/java/Entities/Business/Personne/Acteur.java
+++ src/main/java/Entities/Business/Personne/Acteur.java
@@ -1,140 +1,96 @@
 package Entities.Business.Personne;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="Acteur")
+@Table(name = "Acteur")
 @Data
 @Builder
-public class Acteur implements IPersonne<Long> {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Acteur extends Personne {
 
-    // déclaration des attributs
-
-    // clé primaire, l'id d'acteur
+    // Primary key
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_acteur")
     private Long idActeur;
 
-    // id de personne
+    // IMDB ID
     @Column(name = "id_imdb")
     private long idImdb;
 
-    @Column(name = "nom")
-    private String nom;
-
-    @Column(name = "prenom")
-    private String prenom;
-
-    @Column(name = "date_naissance")
-    private LocalDateTime dateNaissance;
-
+    // Size attribute
     @Column(name = "taille")
     private double taille;
 
+    // Relationship with Role
     @OneToMany(mappedBy = "acteur", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Role> roles;
+    private List<Role> roles = new ArrayList<>();
 
-    // date de création
-    // todo question: is it necessary to create a column or I can skip the mapping?
+    // Creation and update timestamps
+    @Column(name = "created_date")
     private LocalDateTime createdDate;
 
+    @Column(name = "updated_date")
     private LocalDateTime updatedDate;
 
-    // constructeur vide
-    public Acteur() {
-
-    }
-
-    // constructeur avec des parametres
+    // Custom constructor
     public Acteur(long idImdb, String nom, String prenom, LocalDateTime dateNaissance, double taille) {
+        super.setNom(nom);
+        super.setPrenom(prenom);
+        super.setDateNaissance(dateNaissance);
         this.idImdb = idImdb;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.dateNaissance = dateNaissance;
         this.taille = taille;
         this.createdDate = LocalDateTime.now();
-        this.roles = new ArrayList<>();
     }
 
-    // méthodes
-
-    public String getNom() {
-        return this.nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return this.prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public LocalDateTime getDateNaissance() {
-        return this.dateNaissance;
-    }
-
-    public void setDateNaissance(LocalDateTime dateNaissance) {
-        this.dateNaissance = dateNaissance;
-    }
-
+    // Implementations for IEntity methods
+    @Override
     public Long getId() {
         return this.idActeur;
     }
 
-    public void setId(Long aLong) {
-        this.idActeur = idActeur;
+    @Override
+    public void setId(Long id) {
+        this.idActeur = id;
     }
 
-    public long getIdImdb() {
-        return idImdb;
-    }
-
-    public void setIdImdb(long idImdb) {
-        this.idImdb = idImdb;
-    }
-
-    public double getTaille() {
-        return taille;
-    }
-
-    public void setTaille(double taille) {
-        this.taille = taille;
-    }
-
+    @Override
     public LocalDateTime getCreatedDate() {
-        return this.createdDate;
+        return createdDate;
     }
 
+    @Override
     public void setCreatedDate(LocalDateTime createdDate) {
         this.createdDate = createdDate;
     }
 
+    @Override
     public LocalDateTime getUpdatedDate() {
-        return this.updatedDate;
+        return updatedDate;
     }
 
+    @Override
     public void setUpdatedDate(LocalDateTime updatedDate) {
         this.updatedDate = updatedDate;
     }
 
+    @Override
     public boolean isDeleted() {
-        return false;
+        return false; // Modify as needed
     }
 
+    @Override
     public void setDeleted(boolean deleted) {
-        // todo how to do it?
+        // Modify as needed
     }
 }
---
+++ src/main/java/Entities/Business/Personne/Personne.java
@@ -0,0 +1,36 @@
+package Entities.Business.Personne;
+
+import java.time.LocalDateTime;
+
+public abstract class Personne implements IPersonne<Long> {
+
+    private String nom;
+    private String prenom;
+    private LocalDateTime dateNaissance;
+
+    // Getters and Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public LocalDateTime getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(LocalDateTime dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+}
